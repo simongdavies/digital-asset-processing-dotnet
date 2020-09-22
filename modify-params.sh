b69227def096e6d93dfd58ea563e0cafaa550f78
Ralph Squillace <ralph@squillace.com>
2020-09-22 07:49:38 -0400

    using bicep now

--- modify-params.sh
+++ modify-params.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
+sed -i.bu "s/defaultResourceNamePrefix string/defaultResourceNamePrefix string = '${PREFIX}'/g" src/arm/main.bicep
 
-
-jq --arg PREFIX ${DEFAULTRESOURCENAMEPREFIX} --arg STORAGE ${STORAGEACCOUNTNAME} --arg COSACCNAME ${COSMOSACCOUNTNAME} --arg COSCOLLNAME ${COSMOSCOLLECTIONNAME} --arg COSDBNAME ${COSMOSDATABASENAME} --arg APPINS ${APPLICATIONINSIGHTSNAME} --arg FUNCNAME ${FUNCTIONAPPNAME} --arg WEBAPPNAME ${WEBAPPNAME} "${JQ_FILTER}" src/arm/azuredeploy.parameters.json >> src/arm/modified-parameters.json
+#jq --arg PREFIX ${DEFAULTRESOURCENAMEPREFIX} --arg STORAGE ${STORAGEACCOUNTNAME} --arg COSACCNAME ${COSMOSACCOUNTNAME} --arg COSCOLLNAME ${COSMOSCOLLECTIONNAME} --arg COSDBNAME ${COSMOSDATABASENAME} --arg APPINS ${APPLICATIONINSIGHTSNAME} --arg FUNCNAME ${FUNCTIONAPPNAME} --arg WEBAPPNAME ${WEBAPPNAME} "${JQ_FILTER}" src/arm/azuredeploy.parameters.json >> src/arm/modified-parameters.json

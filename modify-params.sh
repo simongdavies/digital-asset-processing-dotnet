8e4fbfef3150f6f43cbc726f96b72fea3fcc2103
Simon Davies <simongdavies@users.noreply.github.com>
2021-02-02 21:08:04 +0000

    updated

--- modify-params.sh
+++ modify-params.sh
@@ -3,5 +3,5 @@ export JQ_FILTER=".parameters.defaultResourceNamePrefix.value=$PREFIX | .paramet
 
 #sed -i.bu -e '' "s/defaultResourceNamePrefix string/defaultResourceNamePrefix string = $DEFAULTRESOURCENAMEPREFIX/g" src/arm/main.bicep
 #cat src
-jq --arg PREFIX ${DEFAULTRESOURCENAMEPREFIX} --arg STORAGE ${STORAGEACCOUNTNAME} --arg COSACCNAME ${COSMOSACCOUNTNAME} --arg COSCOLLNAME ${COSMOSCOLLECTIONNAME} --arg COSDBNAME ${COSMOSDATABASENAME} --arg APPINS ${APPLICATIONINSIGHTSNAME} --arg FUNCNAME ${FUNCTIONAPPNAME} --arg WEBAPPNAME ${WEBAPPNAME} "${JQ_FILTER}" src/arm/azuredeploy.parameters.json >> src/arm/modified-parameters.json
+# jq --arg PREFIX ${DEFAULTRESOURCENAMEPREFIX} --arg STORAGE ${STORAGEACCOUNTNAME} --arg COSACCNAME ${COSMOSACCOUNTNAME} --arg COSCOLLNAME ${COSMOSCOLLECTIONNAME} --arg COSDBNAME ${COSMOSDATABASENAME} --arg APPINS ${APPLICATIONINSIGHTSNAME} --arg FUNCNAME ${FUNCTIONAPPNAME} --arg WEBAPPNAME ${WEBAPPNAME} "${JQ_FILTER}" src/arm/azuredeploy.parameters.json >> src/arm/modified-parameters.json
 #jq --arg PREFIX ${DEFAULTRESOURCENAMEPREFIX} "${JQ_FILTER}" main.json >> modified-template.json
